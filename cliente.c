e31d93693d7b8be589b5a6c3d5d884fdd2bea71c
Matias1608 <josperez@frba.utn.edu.ar>
2025-11-24 15:09:38 -0300

    Refactor cliente.c for improved functionality

--- cliente.c
+++ cliente.c
@@ -2,60 +2,28 @@
 #include "lista.h"
 #include <fcntl.h>
 
+struct nodo* lista_motos=NULL;
+    
+void AutoguardarLista(int signal_number);
+
+
 int main(void)
 {
-    int stockfd;
+    FILE *datos_lista;
     int sockfd_cl;
     int puerto = 1608;
-    char buffer[100];
-    struct nodo* lista_motos, *aux;
     int opcion;
+    char buffer[100];
+    memset(buffer, 0, sizeof(buffer)); 
 
-    memset(&lista_motos, 0, sizeof(lista_motos));
-    memset(&aux, 0, sizeof(aux));
+    signal(SIGALRM, AutoguardarLista);
 
-    stockfd=open("stock.dat", O_RDWR | O_CREAT, 0660);
-    if(stockfd == -1)
+    while (1)
     {
-        perror("Error al abrir el archivo de stock");
-        return -1;
+        alarm(10); // Configura la alarma para 30 minutos
+        pause(); // Espera a que llegue una señal
     }
-    printf("--Menu--\n\n1-Buscar\n2-Venta\n3-Ingreso\n4-Listado\n5-Salir\n");
-    scanf("%d", &opcion);
-    while(opcion<1 || opcion>5)
-    {
-        perror("Error, ingrese una opcion valida (1-5)\n");
-        while(getchar() != '\n')
-        scanf("%d", &opcion);
-    }
-    switch (opcion)
-    {
-    case 1:
-        //Buscar
-        ModificarLista(&lista_motos);
-        break;
-    
-    case 2:
-        //Venta 
-
-        break;
-    
-    case 3:
-        //Ingreso
-
-        break;
-    
-    case 4:
-        //Listado   
-        
-        break;
-    
-    default:
-        break;
-    }
-
-
-    memset(buffer, 0, sizeof(buffer));  
+     
 
     sockfd_cl = ConectarServidor("localhost", puerto);
 
@@ -70,9 +38,66 @@ int main(void)
         printf("Conexion al servidor exitosa\n");
     }
 
-   recv(sockfd_cl, buffer, sizeof(buffer)-1, 0);
+    recv(sockfd_cl, buffer, sizeof(buffer)-1, 0);
 
-   printf("Mensaje del servidor: %s\n", buffer);
+    printf("Mensaje del servidor: %s\n", buffer);
+
+    send(sockfd_cl, "Mensaje del cliente recibido correctamente", 42, 0);
+
+    //lista_motos=NULL;
+    
+
+    datos_lista=fopen("stock.dat", "rb"); 
+    if(datos_lista != NULL)
+    {
+        //Cargar lista desde archivo
+        lista_motos=LeerLista(datos_lista);
+        fclose(datos_lista);
+    }
+    else
+    {
+        printf("No se encontro el archivo stock.dat, se creara uno nuevo al guardar los datos.\n");
+    }
+
+    do
+    {
+        printf("--Menu--\n\n1-Buscar'\'modificar\n2-Listado\n3-Salir y guardar\n");
+        scanf("%d", &opcion);
+        while(opcion<1 || opcion>3)
+        {
+            perror("Error, ingrese una opcion valida (1-3)\n");
+            while(getchar() != '\n')
+            scanf("%d", &opcion);
+        }
+        switch (opcion)
+        {
+        case 1:
+            //Buscar
+            ModificarLista(&lista_motos);
+            break;
+        
+        case 2:
+            //Listado   
+            MostrarListado(lista_motos);
+            break;
+
+        case 3:
+            //Salir y guardar
+            GuardarLista(lista_motos);
+            break;
+
+        default:
+            break;
+        }
+    }while(opcion!=3);
 
     return 0;
+}
+
+void AutoguardarLista(int signal_number)
+{
+    printf("Autoguardando lista de stock...\n");
+    //Codigo para autoguardar la lista
+    GuardarLista(lista_motos);
+
 }
